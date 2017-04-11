5daadab570fd775ffa2483240634da3646ce8322
ping <wild_ghost@yeah.net>
2017-04-11 22:59:06 +0800

    aa

--- src/main/webapp/js/journaling.js
+++ src/main/webapp/js/journaling.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-var ajaxUrl = "http://192.168.0.115:8080/yiwu/";
-//var ajaxUrl = "http://yzw.chenksoft.com:8080/yiwu/";
-=======
+
 //var ajaxUrl = "http://192.168.0.115:8080/yiwu/";
 var ajaxUrl = "http://yzw.chenksoft.com:8080/yiwu/";
->>>>>>> coursetable-jin
 //var ajaxUrl = "";
 var currentDate = new Date();
 var year=currentDate.getFullYear();
