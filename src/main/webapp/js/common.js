5bccf33c1d4dec7cdbaea042284f42ee51be1b4d
king <499905301@qq.com>
2017-04-12 10:39:21 +0800

    to commit

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,7 +1,5 @@
-function getUrlParam(name){   
-    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");  
-    var r = window.location.search.substr(1).match(reg);  
-    if (r!=null)
-    return unescape(r[2]); 
-    return null;  
+function getUrlParam(name){
+var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+var r = window.location.search.substr(1).match(reg);
+if (r!=null) return unescape(r[2]); return null;
 }
