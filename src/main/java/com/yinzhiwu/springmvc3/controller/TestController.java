60992eb78875b383f51d4a2c87fb14099c298648
ping-0123 <wild_ghost@yeah.net>
2017-04-22 13:22:33 +0800

    aa

--- src/main/java/com/yinzhiwu/springmvc3/controller/TestController.java
+++ src/main/java/com/yinzhiwu/springmvc3/controller/TestController.java
@@ -3,6 +3,7 @@ package com.yinzhiwu.springmvc3.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,4 +19,13 @@ public class TestController {
 		}
 		return builder.toString();
 	}
+	
+	@RequestMapping(value="/instance", method=RequestMethod.GET)
+	@ResponseBody
+	public String testObject(@RequestParam String name){
+		System.out.println("testObject executed");
+		return name;
+	}
+	
+	
 }
