bdcc00cf2ae422f5f336dce3693ef33b94574de5
ping-0123 <wild_ghost@yeah.net>
2017-04-22 12:40:13 +0800

    zzz

---
+++ src/main/java/com/yinzhiwu/springmvc3/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.yinzhiwu.springmvc3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+	@ResponseBody
+	@RequestMapping(value="/array", method=RequestMethod.GET)
+	public String testArray(String[] name){
+		StringBuilder builder = new StringBuilder();
+		for (String string : name) {
+			builder.append(string + " " );
+		}
+		return builder.toString();
+	}
+}
