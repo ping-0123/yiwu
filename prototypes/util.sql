a2d013946b68d399ba5052f5a70d9d6bfe5d5471
ping <wild_ghost@yeah.net>
2017-05-02 01:17:29 +0800

    fomat util.sql

--- prototypes/util.sql
+++ prototypes/util.sql
@@ -1,5 +1,5 @@
 -- 生成修改Auto increment 厨师值 ddl
-select concat('alter table ' ,table_name ,' AUTO_INCREMENT = ',1000000 * rowno, ';') from (
-SELECT @ROWNO := @ROWNO + 1 AS ROWNO, t1.* from
- (select * from information_schema.TABLES  where TABLE_SCHEMA = 'yiwu' and TABLE_TYPE = 'BASE TABLE' ) t1,
-       (SELECT @ROWNO := 0) T2) t3;
+select concat('alter table ' ,table_name ,' AUTO_INCREMENT = ',1000000 * rowno, ';')
+from (SELECT @ROWNO := @ROWNO + 1 AS ROWNO, t1.* 
+		FROM (select * from information_schema.TABLES  where TABLE_SCHEMA = 'yiwu' and TABLE_TYPE = 'BASE TABLE' ) t1,(SELECT @ROWNO := 0) t2
+	) t3;
