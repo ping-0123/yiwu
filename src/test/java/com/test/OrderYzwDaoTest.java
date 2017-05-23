7af5527f12dc263ec48cf35efefb8b158085356c
ping-0123 <wild_ghost@yeah.net>
2017-05-23 17:28:36 +0800

    修复 yiwu/api/order/id/{id} API 因为id无效， 和通过id查到到的course_id无效而导致的异常

--- src/test/java/com/test/OrderYzwDaoTest.java
+++ src/test/java/com/test/OrderYzwDaoTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.yinzhiwu.springmvc3.dao.impl.OrderYzwDaoImpl;
+import com.yinzhiwu.springmvc3.entity.yzw.OrderYzw;
 
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -27,4 +28,12 @@ public class OrderYzwDaoTest {
 		System.out.println(calendar.getTime());
 		System.out.println(orderYzwDaoImpl.test_find_produce_commission_orders(calendar.getTime()).size());
 	}
+	
+	@Test
+	public void testGet(){
+		String id = "20160514060-4";
+		OrderYzw order = orderYzwDaoImpl.get(id);
+		System.out.println(order.getId());
+	}
+	
 }
