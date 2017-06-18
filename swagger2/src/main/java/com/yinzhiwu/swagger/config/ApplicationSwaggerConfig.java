1afd45305b80f9f9430b94656d0c8546f8a46afb
ping <wild_ghost@yeah.net>
2017-06-19 01:10:26 +0800

    add swagger demo

---
+++ swagger2/src/main/java/com/yinzhiwu/swagger/config/ApplicationSwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.yinzhiwu.swagger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class ApplicationSwaggerConfig {
+
+	/**
+	 * Every Docket bean is picked up by the swagger-mvc framework - allowing for multiple
+	 * swagger groups i.e same code base multple swagger resource listings.
+	 */
+	@Bean
+	public Docket addUserDocket(){
+		Docket docket = new Docket(DocumentationType.SWAGGER_2);
+		springfox.documentation.service.ApiInfo apiInfo = new ApiInfo(
+				"yiwu API", 
+				"API Document Management", 
+				"v1.0.0",
+				"www.baidu.com",
+				"wild_ghost@yeah.net", 
+				"",
+				"");
+		docket.apiInfo(apiInfo);
+		return docket;
+	}
+}
