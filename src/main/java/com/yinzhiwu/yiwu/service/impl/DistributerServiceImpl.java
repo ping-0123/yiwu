8c743c392b5289cfb61918732fac04e8c44fda40
ping-0123 <wild_ghost@yeah.net>
2017-07-31 16:22:22 +0800

    修复 /api/distributer/getTopThree

--- src/main/java/com/yinzhiwu/yiwu/service/impl/DistributerServiceImpl.java
+++ src/main/java/com/yinzhiwu/yiwu/service/impl/DistributerServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
