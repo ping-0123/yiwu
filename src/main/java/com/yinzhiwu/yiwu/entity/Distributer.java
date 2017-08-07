585d1cf956670dde8c988d04e5109a3d2400a519
ping-0123 <wild_ghost@yeah.net>
2017-08-07 17:19:24 +0800

    aa

--- src/main/java/com/yinzhiwu/yiwu/entity/Distributer.java
+++ src/main/java/com/yinzhiwu/yiwu/entity/Distributer.java
@@ -22,7 +22,6 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
