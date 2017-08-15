34bb54b7878d2bb549bfda76ab494896f4233836
ping-0123 <wild_ghost@yeah.net>
2017-08-15 21:55:41 +0800

    测试cleanWithHoldTimes成功

--- src/test/java/com/test/EContractTest.java
+++ src/test/java/com/test/EContractTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.yinzhiwu.yiwu.dao.AppointmentYzwDao;
+import com.yinzhiwu.yiwu.dao.OrderYzwDao;
 import com.yinzhiwu.yiwu.entity.yzw.AppointmentYzw;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,11 +18,19 @@ import com.yinzhiwu.yiwu.entity.yzw.AppointmentYzw;
 public class EContractTest {
 
 	@Autowired private AppointmentYzwDao appointmentYzwDao;
+	@Autowired private OrderYzwDao orderDao;
 	
 	@Transactional
 	@Test
 	public void testFindLastDayAppointments(){
 		List<AppointmentYzw> appointmentYzws = appointmentYzwDao.findLastDayAppointments();
 		System.err.println(appointmentYzws.size());
+	}
+	
+	@Transactional
+	@Test
+	public void testCleanWithHoldTimes(){
+		int count = orderDao.cleanWithHoldTimes();
+		System.err.println(count);
 	}
 }
