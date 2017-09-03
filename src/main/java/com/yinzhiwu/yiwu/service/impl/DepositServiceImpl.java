4f41ba9296c7acb07312f39aa295b544b7334e50
ping <wild_ghost@yeah.net>
2017-09-03 16:32:09 +0800

    支付定金不生成订单

--- src/main/java/com/yinzhiwu/yiwu/service/impl/DepositServiceImpl.java
+++ src/main/java/com/yinzhiwu/yiwu/service/impl/DepositServiceImpl.java
@@ -7,22 +7,14 @@ import org.springframework.util.Assert;
 import com.yinzhiwu.yiwu.dao.CustomerYzwDao;
 import com.yinzhiwu.yiwu.dao.DepositDao;
 import com.yinzhiwu.yiwu.dao.DistributerDao;
-import com.yinzhiwu.yiwu.dao.OrderPayedMethodYzwDao;
-import com.yinzhiwu.yiwu.dao.ProductYzwDao;
 import com.yinzhiwu.yiwu.entity.Distributer;
 import com.yinzhiwu.yiwu.entity.income.DepositEvent;
 import com.yinzhiwu.yiwu.entity.type.EventType;
 import com.yinzhiwu.yiwu.entity.type.IncomeType;
 import com.yinzhiwu.yiwu.entity.yzw.CustomerYzw;
-import com.yinzhiwu.yiwu.entity.yzw.OrderPayedMethodYzw;
-import com.yinzhiwu.yiwu.entity.yzw.OrderYzw;
-import com.yinzhiwu.yiwu.entity.yzw.PayedMethodYzw;
-import com.yinzhiwu.yiwu.entity.yzw.ProductYzw;
-import com.yinzhiwu.yiwu.entity.yzw.CustomerYzw.CustomerAgeType;
 import com.yinzhiwu.yiwu.exception.YiwuException;
 import com.yinzhiwu.yiwu.service.DepositService;
 import com.yinzhiwu.yiwu.service.IncomeEventService;
-import com.yinzhiwu.yiwu.service.OrderYzwService;
 
 @Service
 public class DepositServiceImpl extends BaseServiceImpl<DepositEvent, Integer> implements DepositService {
@@ -34,12 +26,9 @@ public class DepositServiceImpl extends BaseServiceImpl<DepositEvent, Integer> i
 	private DistributerDao distributerDao;
 	@Autowired private CustomerYzwDao customerYzwDao;
 
-	@Autowired
-	private OrderYzwService orderService;
-	@Autowired private OrderPayedMethodYzwDao orderPayedMethodYzwDao;
 
-	@Autowired
-	private ProductYzwDao productDao;	
+
+
 
 	@Autowired
 	public void setBaseDao(DepositDao depositDao) {
@@ -83,30 +72,30 @@ public class DepositServiceImpl extends BaseServiceImpl<DepositEvent, Integer> i
 		/**
 		 * save deposit order
 		 */
-		ProductYzw product;
-		if (CustomerAgeType.ADULT == distributer.getCustomerAgeType())
-			product = productDao.get_audit_deposit_product();
-		else
-			product = productDao.get_children_deposit_product();
-		OrderYzw order = new OrderYzw(customer, product, amount, distributer.getFollowedByStore());
-		orderService.save(order);
+//		ProductYzw product;
+//		if (CustomerAgeType.ADULT == distributer.getCustomerAgeType())
+//			product = productDao.get_audit_deposit_product();
+//		else
+//			product = productDao.get_children_deposit_product();
+//		OrderYzw order = new OrderYzw(customer, product, amount, distributer.getFollowedByStore());
+//		orderService.save(order);
 		//修改客户定金
 		if(customer.getEarnest() == null)
 			customer.setEarnest(0f);
 		customer.setEarnest(amount + customer.getEarnest() );
 		customerYzwDao.update(customer);
 		//保存支付方式
-		OrderPayedMethodYzw payedMethod = new OrderPayedMethodYzw(order, PayedMethodYzw.INNER_PAYED_METHOD, amount);
-		orderPayedMethodYzwDao.save(payedMethod);
+//		OrderPayedMethodYzw payedMethod = new OrderPayedMethodYzw(order, PayedMethodYzw.INNER_PAYED_METHOD, amount);
+//		orderPayedMethodYzwDao.save(payedMethod);
 		/**
 		 * agaist the order
 		 */
-		float againstAmount = -1 * amount;
-		OrderYzw agaistOrder = new OrderYzw(customer, product, againstAmount, distributer.getFollowedByStore());
-		orderService.save(agaistOrder);
-		OrderPayedMethodYzw againstPayedMethod = new OrderPayedMethodYzw(order, PayedMethodYzw.INNER_PAYED_METHOD, againstAmount);
-		orderPayedMethodYzwDao.save(againstPayedMethod);
-		
+//		float againstAmount = -1 * amount;
+//		OrderYzw agaistOrder = new OrderYzw(customer, product, againstAmount, distributer.getFollowedByStore());
+//		orderService.save(agaistOrder);
+//		OrderPayedMethodYzw againstPayedMethod = new OrderPayedMethodYzw(order, PayedMethodYzw.INNER_PAYED_METHOD, againstAmount);
+//		orderPayedMethodYzwDao.save(againstPayedMethod);
+//		
 		/**
 		 * save deposit event
 		 */
@@ -119,11 +108,11 @@ public class DepositServiceImpl extends BaseServiceImpl<DepositEvent, Integer> i
 			fundsPayed	 = amount>brokeragePayed?amount-brokeragePayed:0f;
 		}
 		if(fundsPayed> 0f){
-			fundsDepoist = new DepositEvent(distributer, EventType.PAY_DEPOSIT_BY_FUNDS, fundsPayed, order);
+			fundsDepoist = new DepositEvent(distributer, EventType.PAY_DEPOSIT_BY_FUNDS, fundsPayed, null);
 			incomeEventService.save(fundsDepoist);
 		}
 		if(brokeragePayed>0f){
-			brokerageDeposit = new DepositEvent(distributer, EventType.PAY_DEPOSIT_BY_BROKERAGE, brokeragePayed, order);
+			brokerageDeposit = new DepositEvent(distributer, EventType.PAY_DEPOSIT_BY_BROKERAGE, brokeragePayed, null);
 			incomeEventService.save(brokerageDeposit);
 		}
 
