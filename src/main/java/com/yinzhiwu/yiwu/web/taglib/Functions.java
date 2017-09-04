49ec4c0a3533d3f14115052d299d3e8b6cceb64b
ping-0123 <wild_ghost@yeah.net>
2017-09-04 12:04:10 +0800

    修复一些错误

--- src/main/java/com/yinzhiwu/yiwu/web/taglib/Functions.java
+++ src/main/java/com/yinzhiwu/yiwu/web/taglib/Functions.java
@@ -1,43 +1,51 @@
 package com.yinzhiwu.yiwu.web.taglib;
 
-import org.springframework.util.CollectionUtils;
-
 import java.util.Collection;
 
+import org.springframework.util.CollectionUtils;
+
+import com.yinzhiwu.yiwu.entity.sys.Resource;
+import com.yinzhiwu.yiwu.entity.sys.Role;
+import com.yinzhiwu.yiwu.entity.yzw.DepartmentYzw;
+import com.yinzhiwu.yiwu.service.DepartmentYzwService;
+import com.yinzhiwu.yiwu.service.ResourceService;
+import com.yinzhiwu.yiwu.service.RoleService;
+import com.yinzhiwu.yiwu.shiro.spring.SpringUtils;
+
 /**
- * <p>User: Zhang Kaitao
- * <p>Date: 14-2-15
- * <p>Version: 1.0
+ * @author ping
+ * @version 2017年9月4日上午11:58:06
+ *
  */
 public class Functions {
 
-    public static boolean in(Iterable iterable, Object element) {
+    public static boolean in(Iterable<?> iterable, Object element) {
         if(iterable == null) {
             return false;
         }
         return CollectionUtils.contains(iterable.iterator(), element);
     }
 
-    public static String organizationName(Long organizationId) {
-        Organization organization = getOrganizationService().findOne(organizationId);
+    public static String organizationName(Integer deptId) {
+        DepartmentYzw organization = getDepartmentService().get(deptId);
         if(organization == null) {
             return "";
         }
         return organization.getName();
     }
 
-    public static String organizationNames(Collection<Long> organizationIds) {
-        if(CollectionUtils.isEmpty(organizationIds)) {
+    public static String departmentNames(Collection<Integer> deptIds) {
+        if(CollectionUtils.isEmpty(deptIds)) {
             return "";
         }
 
         StringBuilder s = new StringBuilder();
-        for(Long organizationId : organizationIds) {
-            Organization organization = getOrganizationService().findOne(organizationId);
-            if(organization == null) {
+        for(Integer id : deptIds) {
+            DepartmentYzw dept = getDepartmentService().get(id);
+            if(dept == null) {
                 return "";
             }
-            s.append(organization.getName());
+            s.append(dept.getName());
             s.append(",");
         }
 
@@ -47,22 +55,22 @@ public class Functions {
 
         return s.toString();
     }
-    public static String roleName(Long roleId) {
-        Role role = getRoleService().findOne(roleId);
+    public static String roleName(Integer roleId) {
+        Role role = getRoleService().get(roleId);
         if(role == null) {
             return "";
         }
         return role.getDescription();
     }
 
-    public static String roleNames(Collection<Long> roleIds) {
+    public static String roleNames(Collection<Integer> roleIds) {
         if(CollectionUtils.isEmpty(roleIds)) {
             return "";
         }
 
         StringBuilder s = new StringBuilder();
-        for(Long roleId : roleIds) {
-            Role role = getRoleService().findOne(roleId);
+        for(Integer roleId : roleIds) {
+            Role role = getRoleService().get(roleId);
             if(role == null) {
                 return "";
             }
@@ -76,21 +84,21 @@ public class Functions {
 
         return s.toString();
     }
-    public static String resourceName(Long resourceId) {
-        Resource resource = getResourceService().findOne(resourceId);
+    public static String resourceName(Integer resourceId) {
+        Resource resource = getResourceService().get(resourceId);
         if(resource == null) {
             return "";
         }
         return resource.getName();
     }
-    public static String resourceNames(Collection<Long> resourceIds) {
+    public static String resourceNames(Collection<Integer> resourceIds) {
         if(CollectionUtils.isEmpty(resourceIds)) {
             return "";
         }
 
         StringBuilder s = new StringBuilder();
-        for(Long resourceId : resourceIds) {
-            Resource resource = getResourceService().findOne(resourceId);
+        for(Integer resourceId : resourceIds) {
+            Resource resource = getResourceService().get(resourceId);
             if(resource == null) {
                 return "";
             }
@@ -105,15 +113,15 @@ public class Functions {
         return s.toString();
     }
 
-    private static OrganizationService organizationService;
+    private static DepartmentYzwService departmentService;
     private static RoleService roleService;
     private static ResourceService resourceService;
 
-    public static OrganizationService getOrganizationService() {
-        if(organizationService == null) {
-            organizationService = SpringUtils.getBean(OrganizationService.class);
+    public static DepartmentYzwService getDepartmentService() {
+        if(departmentService == null) {
+            departmentService = SpringUtils.getBean(DepartmentYzwService.class);
         }
-        return organizationService;
+        return departmentService;
     }
 
     public static RoleService getRoleService() {
