7b28e8c404ddf7881d7e8e33b252e8fd644863d1
ping-0123 <wild_ghost@yeah.net>
2017-09-22 11:14:02 +0800

    aa

--- src/main/java/com/yinzhiwu/yiwu/context/Constants.java
+++ src/main/java/com/yinzhiwu/yiwu/context/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
 	 public static final String CURRENT_USER = "current_user";
 	 public static final String CURRENT_DISTRIBUTER = "current_distributer";
 	 public static final String CURRENT_DISTRIBUTER_VIWE = "current_distributer_view";
+	 public static final String CURRENT_CUSTOMER = "current_customer";
 	 
 	 public static final String CURRENT_EMPLOYEE_USER = "current_employee_user";
 	 public static final String CURRENT_EMPLOYEE = "current_employee";
